eaa51c4f158a5dad827817378ada0f0af3564c03
Carsten Behring <carsten.behring@gmail.com>
2016-01-12 13:57:54 +0100

    added link

--- citrusProductionSurface/data/portugal/program/readNuts3.R
+++ citrusProductionSurface/data/portugal/program/readNuts3.R
@@ -7,14 +7,15 @@ fixCitrinos <-  function(df) {
         mutate(Citrinos=as.numeric(Citrinos))
 } 
 
-readPortugalCsv <- function(name,fileNumber,skip,sliceStart,sliceEnd) {
+readPortugalCsv <- function(name,fileNumber,skip,sliceStart,sliceEnd,link) {
     data <- read_csv(paste0(getwd(),"/portugal/original/",name,"/",fileNumber,".csv"),
                      locale=iso88591Locale(),skip=skip)
     names(data)[2:4] <- c("nuts3","concelho","unit")
     data <- data[,1:8]
     data  %>% select(nuts3,Citrinos) %>%
         slice(sliceStart:(n()+sliceEnd))  %>% 
-        fixCitrinos()
+        fixCitrinos() %>%
+        mutate(link=link)
 }
 
 
@@ -37,14 +38,14 @@ readCitrus2009Census <- function() {
     }
 
     
-    data <- bind_rows(readPortugalCsv("218_RGA Alentejo","020",2,3,-1),
-                      readPortugalCsv("218_RGA Alentejo","021",2,1,-1),
-                      readPortugalCsv("213_RGA EDM","016",2,3,-1),
-                      readPortugalCsv("216_RGA Beira Litoral","019",2,3,-1),
-                      readPortugalCsv("217_RGA Trás Montes","010",2,3,-1),
-                      readPortugalCsv("220_RGA ROeste","017",skip=3,3,-1),
-                      readPortugalCsv("220_RGA ROeste","018",3,3,-1),
-                      readPortugalCsv("RGA-BI_1999","013",2,3,-1)) %>%
+    data <- bind_rows(readPortugalCsv("218_RGA Alentejo","020",2,3,-1,"http://ra09.ine.pt/ngt_server/attachfileu.jsp?look_parentBoui=62669139&att_display=n&att_download=y"),
+                      readPortugalCsv("218_RGA Alentejo","021",2,1,-1,"http://ra09.ine.pt/ngt_server/attachfileu.jsp?look_parentBoui=62669139&att_display=n&att_download=y"),
+                      readPortugalCsv("213_RGA EDM","016",2,3,-1,"http://ra09.ine.pt/ngt_server/attachfileu.jsp?look_parentBoui=5605078&att_display=n&att_download=y"),
+                      readPortugalCsv("216_RGA Beira Litoral","019",2,3,-1,"http://ra09.ine.pt/ngt_server/attachfileu.jsp?look_parentBoui=62668225&att_display=n&att_download=y"),
+                      readPortugalCsv("217_RGA Trás Montes","010",2,3,-1,"http://ra09.ine.pt/ngt_server/attachfileu.jsp?look_parentBoui=62668853&att_display=n&att_download=y"),
+                      readPortugalCsv("220_RGA ROeste","017",skip=3,3,-1,"http://ra09.ine.pt/ngt_server/attachfileu.jsp?look_parentBoui=374399&att_display=n&att_download=y"),
+                      readPortugalCsv("220_RGA ROeste","018",3,3,-1,"http://ra09.ine.pt/ngt_server/attachfileu.jsp?look_parentBoui=374399&att_display=n&att_download=y"),
+                      readPortugalCsv("RGA-BI_1999","013",2,3,-1,"http://ra09.ine.pt/ngt_server/attachfileu.jsp?look_parentBoui=374403&att_display=n&att_download=y")) %>%
         mutate(ha=lead(Citrinos)) %>%
         filter(!is.na(nuts3)) %>%
         rename(name=nuts3) %>%
@@ -53,7 +54,6 @@ readCitrus2009Census <- function() {
                year=2009,
                comment="",
                source="http://ra09.ine.pt",
-               link="",
                nutsVersion="1995PT",
                date="11/01/2016")
     
